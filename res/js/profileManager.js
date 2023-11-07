6c9ed4899cadae4ae2ffd71fbaf27b3332005509
DrSirKnight <Jeepi123456@gmail.com>
2023-11-07 23:05:15 +0200

    3 new visual effects + style improvements

--- res/js/profileManager.js
+++ res/js/profileManager.js
@@ -3,14 +3,35 @@ document.addEventListener("DOMContentLoaded", function() {
     var profilePicture = document.getElementById("openProfile");
     var profile = document.getElementById("profile");
 
-    profilePicture.addEventListener("click", function() {
+    profilePicture.addEventListener("click", function(e) {
+        e.stopPropagation();
+        openProfile();
+    });
+
+    document.body.addEventListener("click", function() {
+        if (profile.style.display === "block") {
+            openProfile();
+        }
+    });
+
+    function openProfile() {
         if (profile.style.display == "none" || profile.style.display == "") {
             profile.style.display = "block";
             var profilePicRect = profilePicture.getBoundingClientRect();
             profile.style.top = profilePicRect.bottom + "px";
-            profile.style.left = profilePicRect.left - profile.offsetWidth + profilePicture.offsetWidth + "px";
+            if (window.innerWidth > 1000) {
+                profile.style.left = profilePicRect.left - profile.offsetWidth + profilePicture.offsetWidth + "px";
+                profile.style.transformOrigin = "top right";
+            } else {
+                profile.style.left = profilePicRect.left - profile.offsetWidth/2 + profilePicture.offsetWidth/2 + "px";
+                profile.style.transformOrigin = "top";
+            }
+            profile.style.transform = "scale(1)";
         } else {
-            profile.style.display = "none";
+            profile.style.transform = "scale(0)";
+            setTimeout(function() {
+                profile.style.display = "none";
+            }, 300);
         }
-    });
+    }
 });
